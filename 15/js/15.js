3758c7aa561a48d4e8f9e828b0b0af9020ac5ab2
huyentrang <huyentrang2518.tdc.edu@gmail.com>
2019-10-15 08:57:40 +0700

    15/10

--- 15/js/15.js
+++ 15/js/15.js
@@ -1,17 +1,19 @@
-$(document).ready(function(){
-  $("#des").click(function(){
-    $("#footwear").css("visibility","visible");
-    $("#review").css("visibility","hidden");
-    $("#deliver").css("visibility","hidden");
+
+$(document).ready(function () {
+  $("#des").click(function () {
+    $("#footwear").css("visibility", "visible");
+    $("#review").css("visibility", "hidden");
+    $("#deliver").css("visibility", "hidden");
   });
-  $("#rev").click(function(){
-    $("#review").css("visibility","visible");
-    $("#footwear").css("visibility","hidden");
-    $("#deliver").css("visibility","hidden");
+  $("#rev").click(function () {
+    $("#review").css("visibility", "visible");
+    $("#footwear").css("visibility", "hidden");
+    $("#deliver").css("visibility", "hidden");
   });
-  $("#ship").click(function(){
-    $("#deliver").css("visibility","visible");
-    $("#footwear").css("visibility","hidden");
-    $("#review").css("visibility","hidden");
+  $("#ship").click(function () {
+    $("#deliver").css("visibility", "visible");
+    $("#footwear").css("visibility", "hidden");
+    $("#review").css("visibility", "hidden");
   });
+
 });
