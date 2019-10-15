faf60ba08cdd10d20e37d02a516d1fa31bfefb50
huyentrang <huyentrang2518.tdc.edu@gmail.com>
2019-10-15 11:07:08 +0700

    15/10

--- 6/6-content.php
+++ 6/6-content.php
@@ -9,30 +9,26 @@ $url_path = str_replace('\\', '/', $url_path);
 ?>
 <div class="type-6">
     <div class="container">
-        <div class="row">
-            <div class="col-md-6 ">
-                <div class="foo-header">
+        <div class="row all-content">
+            <div class="col-md-6 ">     
                     <h3 class="example">Subtitle text example</h3>
                     <h1 class="shoes">why our shoes ?</h1>
-                </div>
-                <div>
                     <p class="content">Cursus mollis justo dis a consectetur libero ac mollis justo quam fames.
                         Inceptos parturient convallis ut quisque morbi parturient. Vestibulum sed tincidunt.</p>
                     <p class="content-two">Vitae pulvinar ut iaculis et a condimentum morbi diam sodales non nam ut a mi praesent
                         a dignissim.Hendrerit dui condimentum eget a suspendisse ad.</p>
-                </div>
                 <div class="btn-read">
                     <button type="button" class="btn">read more</button>
                 </div>
             </div>
             <div class="col-md-6 images">
                 <div class="container">
-                <div class="row">
-                    <div class="col-sm-6 story">
-                        <img src="./images/shoes-story-1" alt="responsive" width="350" height="450">
+                <div class="row pictures">
+                    <div class="col-sm-6">
+                        <img src="./images/shoes-story-1" alt="responsive" class="img-responsive" width="100%" height="100%">
                     </div>
-                    <div class="col-sm-6 story-shoes">
-                        <img src="./images/shoes-story-2" alt="responsive" width="350" height="450">
+                    <div class="col-sm-6">
+                        <img src="./images/shoes-story-2" alt="responsive"class="img-responsive" width="100%" height="100%">
                     </div>
                 </div>
                 </div>
