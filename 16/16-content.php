413f4c8a1f55ec6303decdfcfdf5cf6e26e27764
huyentrang <huyentrang2518.tdc.edu@gmail.com>
2019-10-15 10:46:10 +0700

    15/10

--- 16/16-content.php
+++ 16/16-content.php
@@ -9,125 +9,151 @@ $url_path = str_replace('\\', '/', $url_path);
 ?>
 
 <div class="type-16">
-<div class="container">
-		<div class="row sm">
-			<div class="col-md-3 col-sm-6">
-				<figure class="card card-product">
-					<div class="img-wrap">
-						<a href="#">
-							<img src="images/1.png" >
-						</a>
-						<div class="hover-img">
-                            <img src="images/2.png">
-                            <a class="font-awe" href="#">
-                            <i class="fa fa-heart-o" aria-hidden="true"></i>
-                            </a>
-                            <a class="font-awe" href="#">
-                            <i class="fa fa-refresh" aria-hidden="true"></i>
-                            </a>
-                            <a class="font-awe" href="#">
-                            <i class="fa fa-search-plus" aria-hidden="true"></i>
-                            </a>
-						</div>
-					</div>
-					<figcaption class="info-wrap">
+    <div class="container">
+        <div class="row sm">
+            <div class="col-md-3 col-sm-6">
+                <figure class="card card-product">
+                    <div class="img-wrap">
+                        <img src="images/1.png" alt="responsive" class="img-responsive">
+                        <ul class="font-awe">
+                            <li>
+                                <i class="fa fa-heart-o" aria-hidden="true"></i>
+                            </li>
+                            <li>
+                                <i class="fa fa-refresh" aria-hidden="true"></i>
+                            </li>
+                            <li>
+                                <i class="fa fa-search-plus" aria-hidden="true"></i>
+                            </li>
+                        </ul>
+                        <div class="hover-img">
+                            <img src="images/2.png" alt="responsive" class="img-responsive">
+                        </div>
+                    </div>
+                    <figcaption class="info-wrap">
                         <a href="#" class="title">Backpack double strap</a>
                         <div class="black"></div>
                         <div class="blue"></div>
-						<div class="price-wrap">
-							<div class="wrap-swap">
-								<span class="price-new">£12.00 - £15.00</span>
-								<div class="select-option">
-									<a href="#">🛒Select Options</a>
-								</div>
-							</div>
-							<!-- <del class="price-old">$1980</del> -->
-						</div>
-					</figcaption>
-				</figure>
-			</div>
-			<div class="col-md-3 col-sm-6">
-				<figure class="card card-product">
-					<div class="img-wrap">
-						<a href="#">
-							<img src="images/9.png" >
-						</a>
-						<div class="hover-img">
-							<img src="images/5.png">
-						</div>
-					</div>
-					<figcaption class="info-wrap">
+                        <div class="price-wrap">
+                            <div class="wrap-swap">
+                                <span class="price-new">£12.00 - £15.00</span>
+                                <div class="select-option">
+                                    <a href="#">🛒Select Options</a>
+                                </div>
+                            </div>
+                            <!-- <del class="price-old">$1980</del> -->
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <figure class="card card-product">
+                    <div class="img-wrap">
+                        <img src="images/9.png" alt="responsive" class="img-responsive">
+                        <ul class="font-awe">
+                            <li>
+                                <i class="fa fa-heart-o" aria-hidden="true"></i>
+                            </li>
+                            <li>
+                                <i class="fa fa-refresh" aria-hidden="true"></i>
+                            </li>
+                            <li>
+                                <i class="fa fa-search-plus" aria-hidden="true"></i>
+                            </li>
+                        </ul>
+                        <div class="hover-img">
+                            <img src="images/5.png" alt="responsive" class="img-responsive">
+                        </div>
+                    </div>
+                    <figcaption class="info-wrap">
                         <a href="#" class="title">Backpack sneaker</a>
                         <div class="black"></div>
                         <div class="brown"></div>
                         <div class="red"></div>
-						<div class="price-wrap">
-							<div class="wrap-swap">
-								<span class="price-new">£12.00 - £15.00</span>
-								<div class="select-option">
-									<a href="#">🛒Select Options</a>
-								</div>
-							</div>
-							<!-- <del class="price-old">$1980</del> -->
-						</div>
-					</figcaption>
-				</figure>
-			</div>
-			<div class="col-md-3 col-sm-6">
-				<figure class="card card-product">
-					<div class="img-wrap">
-						<a href="#">
-							<img src="images/7.png" >
-						</a>
-						<div class="hover-img">
-							<img src="images/6.png">
-						</div>
-					</div>
-					<figcaption class="info-wrap">
+                        <div class="price-wrap">
+                            <div class="wrap-swap">
+                                <span class="price-new">£12.00 - £15.00</span>
+                                <div class="select-option">
+                                    <a href="#">🛒Select Options</a>
+                                </div>
+                            </div>
+                            <!-- <del class="price-old">$1980</del> -->
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <figure class="card card-product">
+                    <div class="img-wrap">
+                        <img src="images/7.png" alt="responsive" class="img-responsive">
+                        <ul class="font-awe">
+                            <li>
+                                <i class="fa fa-heart-o" aria-hidden="true"></i>
+                            </li>
+                            <li>
+                                <i class="fa fa-refresh" aria-hidden="true"></i>
+                            </li>
+                            <li>
+                                <i class="fa fa-search-plus" aria-hidden="true"></i>
+                            </li>
+                        </ul>
+                        <div class="hover-img">
+                            <img src="images/6.png" alt="responsive" class="img-responsive">
+                        </div>
+                    </div>
+                    <figcaption class="info-wrap">
                         <a href="#" class="title">Backpack dress</a>
                         <div class="black"></div>
                         <div class="yellow"></div>
                         <div class="green"></div>
                         <div class="blue"></div>
-						<div class="price-wrap">
-							<div class="wrap-swap">
-								<span class="price-new">£12.00 - £15.00</span>
-								<div class="select-option">
-									<a href="#">🛒Select Options</a>
-								</div>
-							</div>
-							<!-- <del class="price-old">$1980</del> -->
-						</div>
-					</figcaption>
-				</figure>
-			</div>
-			<div class="col-md-3 col-sm-6">
-				<figure class="card card-product">
-					<div class="img-wrap">
-						<a href="#">
-							<img src="images/3.png" >
-						</a>
-						<div class="hover-img">
-							<img src="images/4.png">
-						</div>
-					</div>
-					<figcaption class="info-wrap">
+                        <div class="price-wrap">
+                            <div class="wrap-swap">
+                                <span class="price-new">£12.00 - £15.00</span>
+                                <div class="select-option">
+                                    <a href="#">🛒Select Options</a>
+                                </div>
+                            </div>
+                            <!-- <del class="price-old">$1980</del> -->
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <figure class="card card-product">
+                    <div class="img-wrap">
+                        <img src="images/3.png" alt="responsive" class="img-responsive">
+                        <ul class="font-awe">
+                            <li>
+                                <i class="fa fa-heart-o" aria-hidden="true"></i>
+                            </li>
+                            <li>
+                                <i class="fa fa-refresh" aria-hidden="true"></i>
+                            </li>
+                            <li>
+                                <i class="fa fa-search-plus" aria-hidden="true"></i>
+                            </li>
+                        </ul>
+                        <div class="hover-img">
+                            <img src="images/4.png" alt="responsive" class="img-responsive">
+                        </div>
+                    </div>
+                    <figcaption class="info-wrap">
                         <a href="#" class="title">Backpack double strap</a>
                         <div class="brown"></div>
                         <div class="blue"></div>
-						<div class="price-wrap">
-							<div class="wrap-swap">
-								<span class="price-new">£12.00 - £15.00</span>
-								<div class="select-option">
-									<a href="#">🛒Select Options</a>
-								</div>
-							</div>
-							<!-- <del class="price-old">$1980</del> -->
-						</div>
-					</figcaption>
-				</figure>
-			</div>
-
-		</div>
-	</div><!-- container // -->
+                        <div class="price-wrap">
+                            <div class="wrap-swap">
+                                <span class="price-new">£12.00 - £15.00</span>
+                                <div class="select-option">
+                                    <a href="#">🛒Select Options</a>
+                                </div>
+                            </div>
+                            <!-- <del class="price-old">$1980</del> -->
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
+    </div><!-- container // -->
 </div>
