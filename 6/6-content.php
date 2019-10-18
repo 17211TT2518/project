fddc998666ad4fa92e486238a2ac023ec703806d
huyentrang <huyentrang2518.tdc.edu@gmail.com>
2019-10-18 17:46:30 +0700

    18/10

--- 6/6-content.php
+++ 6/6-content.php
@@ -10,13 +10,13 @@ $url_path = str_replace('\\', '/', $url_path);
 <div class="type-6">
     <div class="container">
         <div class="row all-content">
-            <div class="col-md-6 ">     
-                    <h3 class="example">Subtitle text example</h3>
-                    <h1 class="shoes">why our shoes ?</h1>
-                    <p class="content">Cursus mollis justo dis a consectetur libero ac mollis justo quam fames.
-                        Inceptos parturient convallis ut quisque morbi parturient. Vestibulum sed tincidunt.</p>
-                    <p class="content-two">Vitae pulvinar ut iaculis et a condimentum morbi diam sodales non nam ut a mi praesent
-                        a dignissim.Hendrerit dui condimentum eget a suspendisse ad.</p>
+            <div class="col-md-6 ">
+                <h3 class="example">Subtitle text example</h3>
+                <h1 class="shoes">why our shoes ?</h1>
+                <p class="content">Cursus mollis justo dis a consectetur libero ac mollis justo quam fames.
+                    Inceptos parturient convallis ut quisque morbi parturient. Vestibulum sed tincidunt.</p>
+                <p class="content-two">Vitae pulvinar ut iaculis et a condimentum morbi diam sodales non nam ut a mi praesent
+                    a dignissim.Hendrerit dui condimentum eget a suspendisse ad.</p>
                 <div class="btn-read">
                     <button type="button" class="btn">read more</button>
                 </div>
@@ -24,13 +24,25 @@ $url_path = str_replace('\\', '/', $url_path);
             <div class="col-md-6 images-two">
                 <div class="row">
                     <div class="col-md-6">
-                        <img src="./images/shoes-story-1" alt="responsive" class="img-responsive" style="width: 292.5px; height: 337.5px;" >
+                        <img id="myImg" src="./images/shoes-story-1.png" class="img-responsive shoes-left" alt="responsive">
                     </div>
                     <div class="col-md-6">
-                        <img src="./images/shoes-story-2" alt="responsive"class="img-responsive" style="width: 292.5px; height: 337.5px;" >
+                        <img id="myImg" src="./images/shoes-story-2.png" class="img-responsive shoes-right" alt="responsive">
                     </div>
-                </div>      
+                </div>
+            
             </div>
         </div>
+        <div id="myModal" class="modal">
+
+            <!-- The Close Button -->
+            <span class="close">&times;</span>
+
+            <!-- Modal Content (The Image) -->
+            <img class="modal-content" id="img01">
+
+          
+        </div>
     </div>
-</div>
+
+  </div>
--- 6/6.php
+++ 6/6.php
@@ -1,6 +1,6 @@
 <?php
 
-$url_host = 'http://'.$_SERVER['HTTP_HOST'];
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
 $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
 $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
 
@@ -10,7 +10,7 @@ $url_path = str_replace('\\', '/', $url_path);
 
 if (!class_exists('lessc')) {
     $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
-    require_once($dir_block.'/libs/lessc.inc.php');
+    require_once($dir_block . '/libs/lessc.inc.php');
 }
 
 $less = new lessc;
@@ -30,11 +30,17 @@ $less->compileFile('less/6.less', 'css/6.css');
     <link href="<?php echo $url_path ?>/css/bootstrap.min-v3.3.5.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet">
     <link href="<?php echo $url_path ?>/css/6.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url_path ?>/css/swiper.min.css" rel="stylesheet">
+    <link href="<?php echo $url_path ?>/css/animate.css" rel="stylesheet">
+    
 
     <script src="<?php echo $url_path ?>/js/jquery-3.1.1.min.js"></script>
     <script src="<?php echo $url_path ?>/js/bootstrap.min.js"></script>
-<body>
+    <script src="<?php echo $url_path ?>/js/swiper.min.js"></script>
 
+<body>
 <?php include '6-content.php'; ?>
+<script src="<?php echo $url_path ?>/js/6.js"></script>
 </body>
+
 </html>
