6405a66bb8c3fa2aa79e08c2cec642e19f539116
huyentrang <huyentrang2518.tdc.edu@gmail.com>
2019-10-21 10:23:28 +0700

    21/10-Module2

--- 2/2.php
+++ 2/2.php
@@ -22,10 +22,12 @@
     <title>2</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url_path ?>/css/bootstrap-3.3.7.min.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet">
 	<link href="<?php echo $url_path ?>/css/swiper.min.css" rel="stylesheet" type="text/css" />
 	<link href="<?php echo $url_path ?>/css/2.css" rel="stylesheet" type="text/css" />
+	<script src="<?php echo $url_path ?>/js/bootstrap-3.3.7.min.js"></script>
+
 </head>
 
 <body>
