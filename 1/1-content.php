5e2a6fc40b00b9867860c6c2ad7c47221c4c8745
Huyền Trang <huyentrang2518.tdc.edu@users.noreply.github.com>
2019-10-21 07:44:46 +0700

    21/10

--- 1/1-content.php
+++ 1/1-content.php
@@ -183,31 +183,3 @@ $url_path = str_replace('\\', '/', $url_path);
         </div>
     </div>
 </div>
-<!---->
-<div class="type-7">
-    <div class="wp-content">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <h1>Get Discount</h1>
-                </div>
-                <div class="col-md-8">
-                    <form action="">
-                        <div class="form-filed">
-                            <p class="input-icon">
-                                <i class="fa fa-envelope icon"></i>
-                                <input type="email" name="EMAIL" placeholder="Your email address" required=""
-                                    class="input-field">
-                            </p>
-                            <p>
-                                <input type="submit" value="Sign up" class="button-sb">
-                            </p>
-                        </div>
-                    </form>
-
-                </div>
-            </div>
-        </div>
-    </div>
-
-</div>
