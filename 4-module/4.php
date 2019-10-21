f70466fdbaa8f11be787b52033a3913abee5c720
Hoang Van Huy <huynoone@gmail.com>
2019-10-21 14:23:35 +0700

    commit

--- 4-module/4.php
+++ 4-module/4.php
@@ -12,19 +12,20 @@
         require_once($dir_block . '/libs/lessc.inc.php');
     }
     $less = new lessc;
-    $less->compileFile('less/9.less', 'css/9.css');
+    $less->compileFile('less/4.less', 'css/4.css');
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>9</title>
+        <title>4</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="<?php echo $url_path ?>/css/bootstrap-3.3.7.min.css" rel="stylesheet" type="text/css" />
-        <link href="<?php echo $url_path ?>/css/9.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/bootstrap-v4.0.0.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/4.css" rel="stylesheet" type="text/css"/>
         <script src="<?php echo $url_path ?>/js/jquery-3.1.1.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/4-script.js"></script>
     </head>
     <body>
-        <?php include '9-content.php'; ?>
+        <?php include '4-content.php'; ?>
     </body>
 </html>
