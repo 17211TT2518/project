ec02fea360e4186c23b63938039507f201075796
JosHieu <hieunguyenductt6@gmail.com>
2019-10-24 10:24:33 +0700

    script-module-15

--- 15/js/15.js
+++ 15/js/15.js
@@ -1,21 +1,21 @@
+$(document).ready(function() {
+    $("#des").click(function() {
+        $("#footwear").css("visibility", "visible");
+        $("#review").css("visibility", "hidden");
+        $("#deliver").css("visibility", "hidden");
+    });
+    $("#rev").click(function() {
+        $("#review").css("visibility", "visible");
+        $("#footwear").css("visibility", "hidden");
+        $("#deliver").css("visibility", "hidden");
+        $('.save').click(function() {
+            $('#check').prop("checked", true);
+        });
+    });
+    $("#ship").click(function() {
+        $("#deliver").css("visibility", "visible");
+        $("#footwear").css("visibility", "hidden");
+        $("#review").css("visibility", "hidden");
+    });
 
-$(document).ready(function () {
-  $("#des").click(function () {
-    $("#footwear").css("visibility", "visible");
-    $("#review").css("visibility", "hidden");
-    $("#deliver").css("visibility", "hidden");
-  });
-  $("#rev").click(function () {
-    $("#review").css("visibility", "visible");
-    $("#footwear").css("visibility", "hidden");
-    $("#deliver").css("visibility", "hidden");
-  });
-  $("#ship").click(function () {
-    $("#deliver").css("visibility", "visible");
-    $("#footwear").css("visibility", "hidden");
-    $("#review").css("visibility", "hidden");
-  });
-  $('.save').click(function(){
-    $('#check').prop("checked", true);
-  });
 });
