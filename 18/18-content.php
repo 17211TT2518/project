1e7dc1af23a3fd8d50be24147084685260ea15a4
huyentrang <huyentrang2518.tdc.edu@gmail.com>
2019-10-25 12:37:24 +0700

    25/10

--- 18/18-content.php
+++ 18/18-content.php
@@ -9,90 +9,71 @@ $url_path = str_replace('\\', '/', $url_path);
 ?>
 <div class="type-18">
     <div class="container">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <div class="row">
-                            <div class="col-md-6 picture">
-                                <img src="./images/4.png" alt="responsive" style="width: 100%; height: 100%;"
-                                    class="img-responsive">
-                            </div>
-                            <div class="col-md-6 content">
-                                <h1 class="lobor">
-                                    consectetur lobortis meacenas risus mi lacus cond consequat.
-                                </h1>
-                                <p class="varius">
-                                    Suspendisse leo sodales varius leo elementum vestibulum a elit at ultrices a mi a
-                                    praesent enim libero ultrices a amet suscipit. A tempus iaculis habitasse
-                                    suspendisse aenean parturient blandit a rutrum leo elementum purus a a et id non feugiat donec.
-                                </p>
-                                <!-- <img src="./images/austen.png" alt="responsive" class="img-responsive logo"> -->
-                                <div class="all-icon">
-                                    <a href="#"><i id="icon" class="fa fa-facebook" aria-hidden="true"></i></a>
-                                    <a href="#"><i id="icon" class="fa fa-twitter" aria-hidden="true"></i></a>
-                                    <a href="#"><i id="icon" class="fa fa-envelope-o" aria-hidden="true"></i></a>
-                                    <a href="#"><i id="icon" class="fa fa-pinterest-p" aria-hidden="true"></i></a>
-                                    <a href="#"><i id="icon" class="fa fa-instagram" aria-hidden="true"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                    <div class="row">
-                            <div class="col-md-6 picture">
-                                <img src="./images/2.png" alt="responsive" style="width: 100%; height: 100%;"
-                                    class="img-responsive">
-                            </div>
-                            <div class="col-md-6 content">
-                                <h1 class="lobor">
-                                    consectetur lobortis meacenas risus mi lacus cond consequat.
-                                </h1>
-                                <p class="varius">
-                                Suspendisse leo sodales varius leo elementum vestibulum a elit at ultrices a mi a
-                                    praesent enim libero ultrices a amet suscipit. A tempus iaculis habitasse
-                                    suspendisse aenean parturient blandit a rutrum leo elementum purus a a et id non feugiat donec.
-                                </p>
-                                <!-- <img src="./images/austen.png" alt="responsive" class="img-responsive logo"> -->
-                                <div class="all-icon">
-                                    <a href="#"><i id="icon" class="fa fa-facebook" aria-hidden="true"></i></a>
-                                    <a href="#"><i id="icon" class="fa fa-twitter" aria-hidden="true"></i></a>
-                                    <a href="#"><i id="icon" class="fa fa-envelope-o" aria-hidden="true"></i></a>
-                                    <a href="#"><i id="icon" class="fa fa-pinterest-p" aria-hidden="true"></i></a>
-                                    <a href="#"><i id="icon" class="fa fa-instagram" aria-hidden="true"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                    <div class="row">
-                            <div class="col-md-6 picture">
-                                <img src="./images/3.png" alt="responsive" style="width: 100%; height: 100%;"
-                                    class="img-responsive">
-                            </div>
-                            <div class="col-md-6 content">
-                                <h1 class="lobor">
-                                    consectetur lobortis meacenas risus mi lacus cond consequat.
-                                </h1>
-                                <p class="varius">
-                                Suspendisse leo sodales varius leo elementum vestibulum a elit at ultrices a mi a
-                                    praesent enim libero ultrices a amet suscipit. A tempus iaculis habitasse
-                                    suspendisse aenean parturient blandit a rutrum leo elementum purus a a et id non feugiat donec.
-                                </p>
-                                <!-- <img src="./images/austen.png" alt="responsive" class="img-responsive logo"> -->
-                                <div class="all-icon">
-                                    <a href="#"><i id="icon" class="fa fa-facebook" aria-hidden="true"></i></a>
-                                    <a href="#"><i id="icon" class="fa fa-twitter" aria-hidden="true"></i></a>
-                                    <a href="#"><i id="icon" class="fa fa-envelope-o" aria-hidden="true"></i></a>
-                                    <a href="#"><i id="icon" class="fa fa-pinterest-p" aria-hidden="true"></i></a>
-                                    <a href="#"><i id="icon" class="fa fa-instagram" aria-hidden="true"></i></a>
-                                </div>
-                            </div>
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <!-- <img src="./images/logo.png" alt="responsive" class="img-responsive logo"> -->
+                    <img src="./images/5.png" alt="responsive" class="img-responsive">
+                    <div class="text">
+                        <h1 class="lobor">
+                            consectetur lobortis meacenas risus mi lacus cond consequat.
+                        </h1>
+                        <p class="varius">
+                            Suspendisse leo sodales varius leo elementum vestibulum a elit at ultrices a mi a
+                            praesent enim libero ultrices a amet suscipit. A tempus iaculis habitasse
+                        </p>
+                        <div class="all-icon">
+                            <a href="#"><i id="icon" class="fa fa-facebook" aria-hidden="true"></i></a>
+                            <a href="#"><i id="icon" class="fa fa-twitter" aria-hidden="true"></i></a>
+                            <a href="#"><i id="icon" class="fa fa-envelope-o" aria-hidden="true"></i></a>
+                            <a href="#"><i id="icon" class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+                            <a href="#"><i id="icon" class="fa fa-instagram" aria-hidden="true"></i></a>
                         </div>
                     </div>
                 </div>
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-pagination"></div>
+                <div class="swiper-slide">
+                    <img src="./images/4.png" alt="responsive" class="img-responsive">
+                    <div class="text">
+                        <h1 class="lobor">
+                            consectetur lobortis meacenas risus mi lacus cond consequat.
+                        </h1>
+                        <p class="varius">
+                            Suspendisse leo sodales varius leo elementum vestibulum a elit at ultrices a mi a
+                            praesent enim libero ultrices a amet suscipit. A tempus iaculis habitasse
+                        </p>
+                        <div class="all-icon">
+                            <a href="#"><i id="icon" class="fa fa-facebook" aria-hidden="true"></i></a>
+                            <a href="#"><i id="icon" class="fa fa-twitter" aria-hidden="true"></i></a>
+                            <a href="#"><i id="icon" class="fa fa-envelope-o" aria-hidden="true"></i></a>
+                            <a href="#"><i id="icon" class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+                            <a href="#"><i id="icon" class="fa fa-instagram" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <img src="./images/6.png" alt="responsive" class="img-responsive">
+                    <div class="text">
+                        <h1 class="lobor">
+                            consectetur lobortis meacenas risus mi lacus cond consequat.
+                        </h1>
+                        <p class="varius">
+                            Suspendisse leo sodales varius leo elementum vestibulum a elit at ultrices a mi a
+                            praesent enim libero ultrices a amet suscipit. A tempus iaculis habitasse
+                        </p>
+                        <div class="all-icon">
+                            <a href="#"><i id="icon" class="fa fa-facebook" aria-hidden="true"></i></a>
+                            <a href="#"><i id="icon" class="fa fa-twitter" aria-hidden="true"></i></a>
+                            <a href="#"><i id="icon" class="fa fa-envelope-o" aria-hidden="true"></i></a>
+                            <a href="#"><i id="icon" class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+                            <a href="#"><i id="icon" class="fa fa-instagram" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-pagination"></div>
+        </div>
     </div>
+</div>
 </div>
